818fd2374d888633c1d1b22c5009cf4277d52992
Gilhyeon Gim <rlfgus777@gmail.com>
2017-11-14 11:32:21 +0000

    random 당첨자 seed로 확인

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+(1..10).each do |i|
+    User.create(email: "rlfgus#{i}@gmail.com", bgid: "rlfgus#{i}", password: "rlfgus#{i}", password_confirmation: "rlfgus#{i}")
+    (1..5).each do |j|
+        Board.create(user_id: i, boardUser: "rlfgus#{i}@gmail.com", boardUserBGID: "rlfgus#{i}", boardCategory: "Duo", board_create_time: 1510498810)
+    end
+end
